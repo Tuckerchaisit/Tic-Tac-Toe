ddd1d84c7e516fcb52a3ca073c0eef3cd8e549ac
Tucker <tucker.chaisit@gmail.com>
2022-02-03 23:28:29 -0500

    finish psudo1and2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
-
 /*---------------------------- Variables (state) ----------------------------*/
-
-
-
+let board;
+let turn;
+let winner;
 /*------------------------ Cached Element References ------------------------*/
+const msgStatus = document.querySelector("#message");
+const sq = Array.from(document.querySelectorAll(".sq"))
 
 
 
