4169b4ae768c97894cffb19962c1179e3b35ef2b
Tucker <tucker.chaisit@gmail.com>
2022-02-04 00:32:56 -0500

    complete render

--- js/app.js
+++ js/app.js
@@ -8,9 +8,12 @@ let winner; //Use a winner variable to represent three different game states:
 // a player that won
 // a tie has occured
 // or a game that is still in play.
+const playerX = 1;
+const playerO = -1;
 /*------------------------ Cached Element References ------------------------*/
 const msgStatus = document.querySelector("#message"); //Store the element that displays the game status on the page
 const sq = Array.from(document.querySelectorAll(".sq")) //Store the 9 elements that represent the squares on the page
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -28,6 +31,49 @@ function init(){
 }
 
 function render(){
-
+  loopStyle();
+  renderMsg();  
 }
+
+/*----------------------------- Helper Functions ---------------------------*/
+function loopStyle(){
+  for(i=0; i<board.length; i++){
+    if(board[i]===playerX){
+      sq[i].textContent='X';
+    }else{
+      if(board[i]===playerO){
+        sq[i].textContent='O';
+      }
+    }
+  }
+}
+
+function renderMsg(){
+  if(winner === null){ //game is still going
+    renderTurn();
+  }else{
+    if(winner === 'T'){ // game is tie
+      msgStatus.textContent = 'The game is a Tie'
+    }else{
+      if(winner === 1){ // player X has won
+        msgStatus.textContent = 'Congratulations! The Winner is Player X'
+      }else{
+        if(winner === -1){ // player Y has won
+          msgStatus.textContent = 'Congratulations! The Winner is Player O'
+        }
+      }
+    }
+  }
+}
+
+function renderTurn(){ //change the msg to show the current turn while the game is still going
+  if(turn === 1){
+    msgStatus.textContent = 'The current turn is Player X'
+  }else{
+    if(turn === -1){
+      msgStatus.textContent = 'The current turn is Player O'
+    }
+  }
+}
+
 
